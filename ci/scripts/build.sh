5a9105faadcfe3a83452483360b710d1c878790f
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-05-11 12:13:40 +0300

    fixed err where I changed the name of the dir in the pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,7 +5,7 @@ cwd=$(pwd)
 
 export GOPATH=$cwd/go
 
-pushd dp-search-api
+pushd dp-nlp-search-scrubber
   make build-bin && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
