47249528271bdce75acc660fcc34bccd1c7988d5
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-05-23 17:51:43 +0300

    alphabetisized config variables

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eux
 
-
 cwd=$(pwd)
 
 export GOPATH=$cwd/go
--- config/config.go
+++ config/config.go
@@ -8,11 +8,11 @@ import (
 
 // Config represents service configuration for dp-nlp-search-scrubber
 type Config struct {
+	AreaDataFile               string        `envconfig:"AREA_DATA_FILE"`
 	BindAddr                   string        `envconfig:"BIND_ADDR"`
 	GracefulShutdownTimeout    time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
 	HealthCheckInterval        time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
 	HealthCheckCriticalTimeout time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
-	AreaDataFile               string        `envconfig:"AREA_DATA_FILE"`
 	IndustryDataFile           string        `envconfig:"INDUSTRY_DATA_FILE"`
 }
 
@@ -22,11 +22,11 @@ var cfg *Config
 // variables
 func Get() (*Config, error) {
 	cfg = &Config{
+		AreaDataFile:               "data/2011 OAC Clusters and Names csv v2.csv",
 		BindAddr:                   ":28700",
 		GracefulShutdownTimeout:    5 * time.Second,
 		HealthCheckInterval:        30 * time.Second,
 		HealthCheckCriticalTimeout: 90 * time.Second,
-		AreaDataFile:               "data/2011 OAC Clusters and Names csv v2.csv",
 		IndustryDataFile:           "data/SIC07_CH_condensed_list_en.csv",
 	}
 
